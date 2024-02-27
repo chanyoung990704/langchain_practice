221892e90f5a124335bb29d1c678f98c53376f0a
CYPARK <chanyoung990704@naver.com>
2024-02-27 13:11:06 +0900

    Implementing a Chat  chain

--- untitled/tchat/main.py
+++ untitled/tchat/main.py
@@ -1,4 +1,11 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
 
 prompt = ChatPromptTemplate(
     input_variables=["content"],
@@ -7,7 +14,12 @@ prompt = ChatPromptTemplate(
     ]
 
 )
+
+chain = LLMChain(llm=chat, prompt=prompt, output_key="answer")
+
 while True:
     content = input(">> ")
 
-    print(f"you entered : {content}")
+    result = chain({"content": content})
+    print(result["answer"])
+
