fec014b6edbdd544cd7962c5e57f7053a618e43d
CYPARK <chanyoung990704@naver.com>
2024-03-12 16:10:23 +0900

    Generating Embeddings

--- untitled/facts/main.py
+++ untitled/facts/main.py
@@ -1,15 +1,20 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
 
+embeddings = OpenAIEmbeddings()
 text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=0)
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-for doc in docs:
-    print(doc.page_content)
-    print("\n")
+# for doc in docs:
+#     print(doc.page_content)
+#     print("\n")
+
+query = embeddings.embed_query(docs[0].page_content)
+print(query)
