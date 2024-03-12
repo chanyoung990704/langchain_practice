f6435221533dc9cc1cc3c7080f38965ee5b0266e
CYPARK <chanyoung990704@naver.com>
2024-03-12 16:36:59 +0900

    Init - Custom Document Retrievers with ChromaDB

--- untitled/facts/main.py
+++ untitled/facts/main.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,9 +13,17 @@ docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
+db = Chroma.from_documents(docs, embedding=embeddings, persist_directory="emb")
+
+results = db.similarity_search_with_score("what is an interesting English language?", k=2)
+for result in results:
+    print("\n")
+    print(result[1])
+    print(result[0].page_content)
+
 # for doc in docs:
 #     print(doc.page_content)
 #     print("\n")
 
-query = embeddings.embed_query(docs[0].page_content)
-print(query)
+# query = embeddings.embed_query(docs[0].page_content)
+# print(query)
