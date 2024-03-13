c4aa4f6a7317ffe435d9e566b833f5bdf6b20518
CYPARK <chanyoung990704@naver.com>
2024-03-13 12:05:25 +0900

    Creating a Custom Retriever.

---
+++ untitled/facts/redundant_fiter_retriever.py
@@ -0,0 +1,20 @@
+from langchain.embeddings.base import Embeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain.schema.retriever import BaseRetriever
+
+class RedundantFilterRetriever(BaseRetriever):
+    embeddings: Embeddings
+    chroma: Chroma
+
+    def get_relevant_documents(self, query):
+        # 쿼리 스트링의 임베딩 계산
+        emb = self.embeddings.embed_query(query)
+
+        # 임베딩을 이용해 max_marginal_relevance_search_vector 계산
+        return self.chroma.max_marginal_relevance_search_by_vector(
+            embedding=emb,
+            lambda_mult=0.8
+        )
+
+    async def aget_relevant_documents(self):
+        return []
