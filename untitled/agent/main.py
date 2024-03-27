35462c85de73f0792596977c93b04325142e74db
CYPARK <chanyoung990704@naver.com>
2024-03-27 12:21:01 +0900

    Adding Table Context.

--- untitled/agent/main.py
+++ untitled/agent/main.py
@@ -1,15 +1,22 @@
 from langchain.chat_models.openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.schema.messages import SystemMessage
 
 from dotenv import load_dotenv
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables
 
 load_dotenv()
 
 chat = ChatOpenAI()
-prompt = ChatPromptTemplate(messages=[HumanMessagePromptTemplate.from_template("{input}"),
-                                      MessagesPlaceholder(variable_name="agent_scratchpad")])
+
+tables = list_tables()
+
+prompt = ChatPromptTemplate(messages=[
+    SystemMessage(content=f"You are an AI that has access to a SQLite database.\n{tables}"),
+    HumanMessagePromptTemplate.from_template("{input}"),
+    MessagesPlaceholder(variable_name="agent_scratchpad")
+])
 
 tools = [run_query_tool]
 
--- untitled/agent/tools/sql.py
+++ untitled/agent/tools/sql.py
@@ -4,6 +4,13 @@ from langchain.tools import Tool
 connect = sqlite3.connect("db.sqlite")
 
 
+def list_tables():
+    cursor = connect.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = cursor.fetchall()
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
 def run_sqlite_query(query):
     c = connect.cursor()
     try:
