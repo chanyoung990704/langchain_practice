32222a27c1372e532be1d68f4b7929dfe53e6db2
CYPARK <chanyoung990704@naver.com>
2024-03-27 16:04:51 +0900

    Preserving Messages with Agent Executor.

--- untitled/agent/main.py
+++ untitled/agent/main.py
@@ -2,6 +2,7 @@ from langchain.chat_models.openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferMemory
 
 from dotenv import load_dotenv
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
@@ -18,13 +19,16 @@ prompt = ChatPromptTemplate(messages=[
                            f"The database has tables of : {tables}\n"
                            "Do not make any assumptions about what tables exist "
                            "or what columns exist. Instead, use the 'describe_tables' function")),
+    MessagesPlaceholder(variable_name="chat_history"),
     HumanMessagePromptTemplate.from_template("{input}"),
     MessagesPlaceholder(variable_name="agent_scratchpad")
 ])
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
-agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
+agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools, memory=memory)
 
-agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+agent_executor("How many orders are there? Write the result to an html report.")
+agent_executor("Repeat the exact same process for users.")
