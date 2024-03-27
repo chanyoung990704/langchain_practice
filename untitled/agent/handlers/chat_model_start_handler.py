5bb17ad99dbc3992f4da1d268858e8d520879b22
CYPARK <chanyoung990704@naver.com>
2024-03-27 16:43:02 +0900

    More Handler Implementation.

--- untitled/agent/handlers/chat_model_start_handler.py
+++ untitled/agent/handlers/chat_model_start_handler.py
@@ -4,6 +4,16 @@ from uuid import UUID
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain_core.messages import BaseMessage
 
+from pyboxen import boxen
+
+print(
+    boxen("TEXT HERE", title="Human", color="yellow")
+)
+
+def boxen_print(*args, **kwargs):
+    print(boxen(*args, **kwargs))
+
+boxen_print("asdasdasdas", title="asd", color="red")
 
 class ChatModelStartHandler(BaseCallbackHandler):
     def on_chat_model_start(
@@ -17,4 +27,22 @@ class ChatModelStartHandler(BaseCallbackHandler):
         metadata: Optional[Dict[str, Any]] = None,
         **kwargs: Any,
     ) -> Any:
-        print(messages)
+        print("\n\n\n\n=========Sending Messages =============\n\n")
+
+        for message in messages[0]:
+            if message.type == "system":
+                boxen_print(message.content, title=message.type, color="yellow")
+            elif message.type == "human":
+                boxen_print(message.content, title=message.type, color="green")
+            elif message.type == "ai" and "function_call" in message.additional_kwargs:
+                call = message.additional_kwargs["function_call"]
+                boxen_print(f"Running Tool {call['name']} with args {call['arguments']}",
+                            title=message.type,
+                            color="cyan")
+            elif message.type == "ai":
+                boxen_print(message.content, title=message.type, color="blue")
+            elif message.type == "function":
+                boxen_print(message.content, title=message.type, color="purple")
+            else:
+                boxen_print(message.content, title=message.type)
+
